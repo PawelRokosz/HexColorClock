69c1b1022152197d993a404c48f26bdb061ffefe
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-01-30 16:37:52 +0100

    changeTime() function added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
-var time = new Date();
-console.log(time);
+var clock = document.querySelector('#clock');
+var hexColor = document.querySelector('#hex-color');
+
+function changeTime(){
+  var time = new Date();
+  clock.textContent = time.getHours() + ' : ' + time.getMinutes() + ' . ' + time.getSeconds();
+}
+
+changeTime();
